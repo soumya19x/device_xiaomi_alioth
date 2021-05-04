5d2e688fa1fb1796fab4f60cbbd4387452eda443
soumyo19 <fsoummya@gmail.com>
2021-05-04 15:57:10 +0000

    alioth: Initialize AncientOS Changes for alioth

--- ancient_alioth.mk
+++ ancient_alioth.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2020 The Aosp Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,17 +8,29 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := ancient_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# Inherit some AncientOS stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_FOD_ANIMATIONS := true
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
